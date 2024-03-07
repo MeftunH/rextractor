c8a85f014aae64bafcee13291b9897758b0b55cc
MeftunH <maftunhashimli@gmail.com>
2024-03-08 00:00:56 +0300

    FEAT: add specialized dataset function

--- base/load_data.py
+++ base/load_data.py
@@ -15,3 +15,21 @@ class RE_Dataset(torch.utils.data.Dataset):
 
   def __len__(self):
     return len(self.labels)
+
+  class RE_special_Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, pair_dataset, labels, entity_type):
+      self.pair_dataset = pair_dataset
+      self.labels = labels
+      self.entity_type = entity_type
+
+    def __getitem__(self, idx):
+      item = {key: val[idx].clone().detach() for key, val in self.pair_dataset.items()}
+      item['labels'] = torch.tensor(self.labels[idx])
+      item['subject_type'] = self.entity_type['subject_type'].iloc[idx]
+      item['object_type'] = self.entity_type['object_type'].iloc[idx]
+
+      return item
+
+    def __len__(self):
+      return len(self.labels)
