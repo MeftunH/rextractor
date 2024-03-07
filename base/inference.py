3e3f0971583b2e5d79cfa94e1fcd1e2c3a7230ea
MeftunH <maftunhashimli@gmail.com>
2024-03-07 23:47:07 +0300

    ADD:inference

---
+++ base/inference.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+import DataLoader
+import torch
+import tqdm
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+def inference(model, tokenized_sent, device):
+  dataloader = DataLoader(tokenized_sent, batch_size=16, shuffle=False)
+  model.eval()
+  output_pred = []
+  output_prob = []
+  for i, data in enumerate(tqdm(dataloader)):
+    with torch.no_grad():
+      outputs = model(
+          input_ids=data['input_ids'].to(device),
+          attention_mask=data['attention_mask'].to(device),
+          token_type_ids=data['token_type_ids'].to(device)
+          )
+    logits = outputs[0]
+    prob = F.softmax(logits, dim=-1).detach().cpu().numpy()
+    logits = logits.detach().cpu().numpy()
+    result = np.argmax(logits, axis=-1)
+
+    output_pred.append(result)
+    output_prob.append(prob)
+
+  return np.concatenate(output_pred).tolist(), np.concatenate(output_prob, axis=0).tolist()
