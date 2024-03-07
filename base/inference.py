520e3d8fcdb6c6f76467a06e5d95d10ede8f62e6
MeftunH <maftunhashimli@gmail.com>
2024-03-07 23:58:05 +0300

    FEAT: set prediction algorithm's skeleton

--- base/inference.py
+++ base/inference.py
@@ -59,3 +59,47 @@ def load_test_dataset(dataset_dir, tokenizer, model_type):
       tokenized_test, entity_type = punct_tokenized_dataset(test_dataset, tokenizer)
       return test_dataset['id'], tokenized_test, test_label
 
+def main(CFG):
+
+  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+  Tokenizer_NAME = CFG['MODEL_NAME']
+  tokenizer = AutoTokenizer.from_pretrained(Tokenizer_NAME)
+  tokenizer = add_token(tokenizer, CFG['MODEL_TYPE'])
+  MODEL_NAME = CFG['MODEL_SAVE_DIR']
+  model_config = AutoConfig.from_pretrained(MODEL_NAME)
+  test_dataset_dir = CFG['TEST_PATH']
+
+  if CFG['MODEL_TYPE'] == 'base':
+    tokenizer = AutoTokenizer.from_pretrained(Tokenizer_NAME)
+
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+
+    test_id, test_dataset, test_label = load_test_dataset(test_dataset_dir, tokenizer)
+    Re_test_dataset = RE_Dataset(test_dataset ,test_label)
+
+  elif CFG['MODEL_TYPE'] == 'entity_special':
+    model = SepecialEntityBERT(Tokenizer_NAME, model_config, tokenizer) # custom model 에는 내부에 from_pretrained 함수가 없다.
+    state_dict = torch.load(f'{MODEL_NAME}/pytorch_model.bin')
+    model.load_state_dict(state_dict)
+
+    test_id, test_dataset, test_label, entity_type = load_test_dataset(test_dataset_dir, tokenizer)
+    Re_test_dataset = RE_special_Dataset(test_dataset ,test_label, entity_type)
+
+  elif CFG['MODEL_TYPE'] == 'entity_punct':
+    model = SepecialPunctBERT(Tokenizer_NAME, model_config, tokenizer)
+    state_dict = torch.load(f'{MODEL_NAME}/pytorch_model.bin')
+    model.load_state_dict(state_dict)
+
+    test_id, test_dataset, test_label = load_test_dataset(test_dataset_dir, tokenizer)
+    Re_test_dataset = RE_Dataset(test_dataset ,test_label)
+
+  model.to(device)
+
+  pred_answer, output_prob = inference(model, Re_test_dataset, device)
+  pred_answer = num_to_label(pred_answer)
+
+  output = pd.DataFrame({'id':test_id,'pred_label':pred_answer,'probs':output_prob,})
+
+  output.to_csv('./prediction/submission.csv', index=False)
+
+  print('---- Finish! ----')
