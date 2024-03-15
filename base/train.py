dddbb5bbae818052d31b1657629b1186d622ac89
MeftunH <maftunhashimli@gmail.com>
2024-03-15 22:34:51 +0300

    FEAT: add label to number

--- base/train.py
+++ base/train.py
@@ -64,3 +64,11 @@ def compute_metrics(pred):
       'auprc' : auprc,
       'accuracy': acc,
   }
+def label_to_num(label):
+  num_label = []
+  with open('dict_label_to_num.pkl', 'rb') as f:
+    dict_label_to_num = pickle.load(f)
+  for v in label:
+    num_label.append(dict_label_to_num[v])
+
+  return num_label
